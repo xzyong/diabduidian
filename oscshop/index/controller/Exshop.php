7be031d67c60c235729363f6b605130598ab197f
THINK-Lx\THINK <1004618428@qq.com>
2017-09-19 09:38:56 +0800

    添加信息注释

--- oscshop/index/controller/Exshop.php
+++ oscshop/index/controller/Exshop.php
@@ -22,9 +22,13 @@ class Exshop extends HomeBase
     public function index()
     {
 		$this->assign('empty', '~~暂无数据');
+		
 		$this->assign('banner',Db::name('ads_items')->where('ad_id',6)->select());
-
-		$this->assign('cate',Db::name('category')->where('pid',37)->select());
+		//轮播图实例化
+		
+		$this->assign('cate',Db::name('category')->where('pid',0)->select());
+		//分类列表
+		
 		$this->assign('cat',osc_goods()->get_category_goods(37));
 		$this->assign('list',Db::name('goods')->where(['is_points_goods'=>1,'status'=>1])->order("goods_id desc")->limit(6)->select());
 		$this->assign('SEO',['title'=>config('SITE_TITLE'),'keywords'=>config('SITE_KEYWORDS'),'description'=>config('SITE_DESCRIPTION')]);
